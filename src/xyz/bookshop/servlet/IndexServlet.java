b98aeceefd17021535011d17d680d1f362f9e009
whitney <wendydyx@sina.com>
2017-06-13 22:35:19 +0800

    rm Name

--- src/xyz/bookshop/servlet/IndexServlet.java
+++ src/xyz/bookshop/servlet/IndexServlet.java
@@ -1,6 +1,8 @@
 package xyz.bookshop.servlet;
 
 
+import xyz.bookshop.dao.UserDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
 
 @WebServlet(name = "indexServlet", urlPatterns = {"", "/index"})
 public class IndexServlet extends MapJspServlet {
@@ -24,6 +27,7 @@ public class IndexServlet extends MapJspServlet {
     @Override
     protected void initRequest(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         Object user = session.getAttribute("user");
+        UserDao u= new UserDao();
         request.setAttribute("data", new Object[]{"ele0", 1888, -2});
         request.setAttribute("user", user);
     }
